1f42ee2c77a09b649053f944ae8a77c927bc49ed
nguyennhatkhanh206@gmail.com <nguyennhatkhanh206@gmail.com>
2016-11-06 20:58:48 +0700

    Test

--- src/main/java/com/spring/boot/sample/App.java
+++ src/main/java/com/spring/boot/sample/App.java
@@ -13,6 +13,7 @@ public class App
     public static void main( String[] args ) { 
     	System.out.println("Hello Spring Boot ..."); 
     	
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(LoginController.class, args);
     }
+    
 }
