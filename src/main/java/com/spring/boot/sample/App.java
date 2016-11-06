9eb57df4198df5281fbea7f4e399ca3f841ea083
nguyennhatkhanh206@gmail.com <nnsan1989@gmail.com>
2016-11-06 20:53:12 +0700

    New

--- src/main/java/com/spring/boot/sample/App.java
+++ src/main/java/com/spring/boot/sample/App.java
@@ -11,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App 
 {
     public static void main( String[] args ) { 
+    	System.out.println("Hello Spring Boot ..."); 
+    	
         SpringApplication.run(App.class, args);
     }
 }
